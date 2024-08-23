be76d7cb824fbc6a2e9c1ad90c423ce22fe2a027
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-08-23 14:30:18 +0000

    updates

--- src/routes/user/user.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../../controller/user/user.controller");
-const router = (0, express_1.Router)();
-router.get('/referrals/:referalId', user_controller_1.getReferals);
-router.patch('/', user_controller_1.creditReferer);
-exports.default = router;
