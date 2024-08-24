f85e44c3c9fe6a558b96e575aee160a0d55c3d26
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-08-24 21:09:14 +0000

    transaction

--- src/controller/user/auth.controller.ts
+++ src/controller/user/auth.controller.ts
@@ -1,11 +1,11 @@
 import User from "../../model/user.model";
 import { Response, Request } from "express"
-import { requestOtp } from "../../services/auth/requestOtp";
+import { requestOtp, deleteOtp } from "../../services/auth/requestOtp";
 import { handle500Errors } from "../../util/api-response";
 import { verifyOtp } from "../../services/auth/verifyOtp";
 import { generateAcctID, generateReferalId } from "../../services/auth/generateId";
 import jwt from 'jsonwebtoken';
-
+import mongoose from "mongoose";
 
 
 
@@ -24,10 +24,11 @@ const sendOtp = async (req: Request, res: Response) => {
 
 
 const newUser = async (req: Request, res: Response) => {
+    const session = await mongoose.startSession();
+    session.startTransaction();
 
+    const { email, inputCode, referalCode } = req.body;
     try {
-
-        const { email, inputCode, referalCode } = req.body;
         
         const isOtpValid = await verifyOtp(email, inputCode);
 
@@ -48,28 +49,36 @@ const newUser = async (req: Request, res: Response) => {
             })
     
             if (referalCode) {
-                await User.updateOne(
-                    { referalCode: referalCode },
+            const referringUser = await User.findOne({ referalId: referalCode });
+
+            if(!referringUser) return res.status(400).json({ msg: 'Invalid referal code' });
+                await referringUser.updateOne(
+                    { referalId: referalCode },
                     { $push: { referals: newUser._id } }
                 );
             }
 
-            const accessToken = jwt.sign({ email: newUser.email, id: newUser._id, isAdmin: newUser.isAdmin }, 'jwtkey', { expiresIn: '1month' });
+            const accessToken = jwt.sign({ email: newUser.email, id: newUser._id, isAdmin: newUser.isAdmin }, 'jwtkey', { expiresIn: '15d' });
 
             const { isAdmin, ...userDetails } = newUser._doc;
             
             return res.status(201).json({ user: userDetails, accessToken });
         } else {
 
-            const accessToken = jwt.sign({email: user.email, id: user._id}, 'jwtkey', {expiresIn: '1month'});
+            const accessToken = jwt.sign({email: user.email, id: user._id}, 'jwtkey', {expiresIn: '30d'});
 
             const { isAdmin, ...userDetails } = user._doc;
 
             return res.status(200).json({ user: userDetails, accessToken });
         }
         
-    } catch (error) {
-        handle500Errors(error, res)
+    } catch (error: any) {
+        await session.abortTransaction();
+       res.status(500).json(error.message);
+        
+    } finally {
+        session.endSession();
+        await deleteOtp(email);
     }
     
 }
