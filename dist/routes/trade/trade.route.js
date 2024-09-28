f5cf19d4601de0fe8d6207820490b82b0bd131eb
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2024-09-28 09:37:57 +0000

    dist

--- dist/routes/trade/trade.route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const trade_contoller_1 = require("../../controller/trade/trade.contoller");
-const router = (0, express_1.Router)();
-router.route('/create').post(trade_contoller_1.createTrade);
-router.route('/:userId').get(trade_contoller_1.tradeHistory);
-exports.default = router;
